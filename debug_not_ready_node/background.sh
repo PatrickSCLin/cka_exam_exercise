f128317ada3fa6ffa6f92e4dabc5a27b30b8f072
Patrick Lin <patricksclin@gmail.com>
2024-11-14 19:40:27 +0800

    test

--- debug_not_ready_node/background.sh
+++ debug_not_ready_node/background.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+echo "Initializing environment..."
+while ! ssh node01 "systemctl status kubelet" &>/dev/null; do 
+    echo "Waiting for kubelet service to be available..."
+    sleep 2
+done
+
 ssh node01 sudo systemctl stop kubelet
 ssh node01 sudo systemctl disable kubelet
+echo "Node01 kubelet has been disabled"
