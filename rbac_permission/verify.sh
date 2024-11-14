0a2d6b774fcedf867751326b8997d0ad5e21d23f
Patrick Lin <patricksclin@gmail.com>
2024-11-14 21:10:17 +0800

    test

--- rbac_permission/verify.sh
+++ rbac_permission/verify.sh
@@ -1,15 +1,23 @@
-if ! kubectl auth can-i get pods --as=system:serviceaccount:cicd:cicd-token &>/dev/null; then
+if ! kubectl auth can-i get pods --as=system:serviceaccount:cicd:cicd-token -n cicd &>/dev/null; then
     exit 1
 fi
 
-if ! kubectl auth can-i get daemonset --as=system:serviceaccount:cicd:cicd-token &>/dev/null; then
+if ! kubectl auth can-i get daemonset --as=system:serviceaccount:cicd:cicd-token -n cicd &>/dev/null; then
     exit 1
 fi
 
-if ! kubectl auth can-i get statefulset --as=system:serviceaccount:cicd:cicd-token &>/dev/null; then
+if ! kubectl auth can-i get statefulset --as=system:serviceaccount:cicd:cicd-token -n cicd &>/dev/null; then
     exit 1
 fi
 
 if kubectl auth can-i get pods --as=system:serviceaccount:cicd:cicd-token -n default &>/dev/null; then
+    exit 1
+fi
+
+if kubectl get sa cicd-token -n cicd &>/dev/null; then
+    exit 1
+fi
+
+if ! kubectl get rolebinding -n cicd | grep cicd-token | grep cicd-cluster-role &>/dev/null; then
     exit 1
 fi
